3946171348151ec3a2f793ca3cd8bc6d869313ac
Tommy Mikkelsen <tm@orstrand.dk>
2014-12-03 21:45:16 +0100

    V0.0.2.0 See Changelog

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -11,7 +11,7 @@
 #
 
 ######################################### Global Variables #########################################
-PLUGIN_VERSION = '0.0.2.0B ******** DEV VERSION *********'
+PLUGIN_VERSION = '0.0.2.0'
 
 ######################################### Imports ##################################################
 import os
@@ -30,9 +30,9 @@ from chared.detector import list_models, get_model_path, EncodingDetector
 
 ######################################## Start of plugin ###########################################
 def Start():
-	Log.Info(L('Starting') + ' %s ' %(L('Srt2Utf-8')) + L('with a version of') + ' %s' %(PLUGIN_VERSION))
-	print L('Starting') + ' %s ' %(L('Srt2Utf-8')) + L('with a version of') + ' %s' %(PLUGIN_VERSION)
-	print("********  Started on %s  **********" %(Platform.OS))
+	Log.Info(L('Starting') + ' %s ' %(L('Srt2Utf-8')) + L('with a version of') + ' %s on %s' %(PLUGIN_VERSION, Platform.OS))
+#	print L('Starting') + ' %s ' %(L('Srt2Utf-8')) + L('with a version of') + ' %s' %(PLUGIN_VERSION)
+#	print("********  Started on %s  **********" %(Platform.OS))
 	
 ####################################### Movies Plug-In #############################################
 class srt2utf8AgentMovies(Agent.Movies):
